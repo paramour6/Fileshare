e21e33d05b8de561aa9291e5e07dd1dea3d87b2b
Seth Freeman <paramour6ix@gmail.com>
2025-10-16 01:40:16 -0600

    Created DDL script to initialize database schema

---
+++ DDL.sql
@@ -0,0 +1,47 @@
+-- Create database if it doesn't exist and use it
+CREATE DATABASE IF NOT EXISTS fileshare;
+USE fileshare;
+
+-- Create Users table
+CREATE TABLE Users (
+	user_id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(36) UNIQUE NOT NULL,
+    email_address VARCHAR(256) UNIQUE NOT NULL,
+    password_hash VARCHAR(256) NOT NULL
+);
+
+-- Create Collections table
+CREATE TABLE Collections (
+	collection_id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    user_id BIGINT NOT NULL,
+    private BOOL NOT NULL,
+    created_at DATE NOT NULL,
+    CONSTRAINT fk_user_id FOREIGN KEY(user_id)
+    REFERENCES Users(user_id)
+    ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- Create Files table
+CREATE TABLE Files (
+	file_id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    collection_id BIGINT NOT NULL,
+    file_name VARCHAR(256) NOT NULL,
+    file_size_kb INT NOT NUll,
+    hashed_file_name VARCHAR(256) UNIQUE NOT NULL,
+    hash_salt VARCHAR(16) NOT NULL,
+    CONSTRAINT fk_collection_id FOREIGN KEY(collection_id)
+    REFERENCES Collections(collection_id)
+    ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- Create SharedCollections table
+CREATE TABLE SharedCollections (
+	collection_id BIGINT NOT NULL,
+    user_id BIGINT NOT NULL,
+    CONSTRAINT fk_shared_collection_id FOREIGN KEY(collection_id)
+    REFERENCES Collections(collection_id)
+    ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT fk_shared_with_user_id FOREIGN KEY(user_id)
+    REFERENCES Users(user_id)
+    ON DELETE CASCADE ON UPDATE CASCADE
+);
