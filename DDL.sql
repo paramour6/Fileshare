aa832631dba3a768e895df7ed69d913a08df3eae
Seth Freeman <paramour6ix@gmail.com>
2025-10-16 21:58:48 -0600

    Fix typo

--- DDL.sql
+++ DDL.sql
@@ -27,7 +27,7 @@ CREATE TABLE Files (
     collection_id BIGINT NOT NULL,
     file_name VARCHAR(256) NOT NULL,
     file_type VARCHAR(64) NOT NULL,
-    file_size_kb INT NOT NUll,
+    file_size_kb INT NOT NULL,
     hashed_file_name VARCHAR(256) UNIQUE NOT NULL,
     hash_salt VARCHAR(16) NOT NULL,
     CONSTRAINT fk_collection_id FOREIGN KEY(collection_id)
