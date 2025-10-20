8c60db6cfe7d8a5e8ea3fe1025c1d94aa08c125f
Seth Freeman <paramour6ix@gmail.com>
2025-10-19 19:07:09 -0600

    Added password encoder bean

--- src/main/java/com/gcu/fileshare/ApplicationConfiguration.java
+++ src/main/java/com/gcu/fileshare/ApplicationConfiguration.java
@@ -1,9 +1,15 @@
 package com.gcu.fileshare;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 public class ApplicationConfiguration
 {
-    
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder()
+    {
+        return new BCryptPasswordEncoder();
+    }
 }
