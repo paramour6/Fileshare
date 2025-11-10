893b3056afdf59c7a909354c95019ac9657ca6ec
Seth Freeman <paramour6ix@gmail.com>
2025-11-09 19:23:41 -0700

    test

--- frontend/src/views/home/HomeView.tsx
+++ frontend/src/views/home/HomeView.tsx
@@ -6,7 +6,7 @@ function HomeView(): React.ReactElement
     
     return (
         <div>
-            <img src="favicon512.png" width={360} height={360} alt="Fileshare Logo" />
+            <img src="favicon512.png" width={412} height={412} alt="Fileshare Logo" />
             <h1>Welcome to Fileshare!</h1>
             <h4>By Seth Freeman, Dennis Araiza, and Marc Reyes</h4>
         </div>
